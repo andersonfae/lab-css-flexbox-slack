c7468bc3d7cab8ffb11b7001b04200732651acd1
Anderson Fae <andersonfae.owl@gmail.com>
2022-06-01 00:11:47 -0300

    Second LAB - Day 1

---
+++ starter_code/javascript/index.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//   var x = document.getElementById("navHamburguer");
+//   if (x.className === "navbar") {
+//     x.className += "responsive";
+//   } else {
+//     x.className = "navbar";
+//   }
+// }
